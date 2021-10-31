a3dcd10e7811afdccad8e0dc081c5f1a1a46cf56
Danillo Souza <danillosouza1704@gmail.com>
2021-10-31 12:17:31 -0300

    Adicionando codigos de base do GitLab do professor

---
+++ cliente_esp/main/flash_memo.h
@@ -0,0 +1,17 @@
+#ifndef FLASH_MEMO_H
+#define FLASH_MEMO_H
+
+#include <stdio.h>
+#include <string.h>
+#include "esp_log.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+int32_t le_valor_nvs();
+
+void grava_valor_nvs(int32_t valor);
+
+#endif
