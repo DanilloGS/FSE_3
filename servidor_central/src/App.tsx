a439c8e2887e0d186d9a871e3984465149fbb64d
Fellipe <araujo.fellipe@hotmail.com>
2021-11-03 22:07:27 -0300

    Developed initial home page.

--- servidor_central/src/App.tsx
+++ servidor_central/src/App.tsx
@@ -1,10 +1,40 @@
-import './App.css';
-import './global.css';
+import { useState } from 'react';
+import './App.scss';
+import './global.scss';
+
+import { FiPlus, FiDownload } from 'react-icons/fi';
+
+import Button from './components/Button';
+import ModalApp from './components/ModalApp';
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleModal() {
+    setModalIsOpen(!modalIsOpen);
+  }
+
   return (
-    <div className="App">
-      <h1>FSE Trabalho 3</h1>
+    <div id="home">
+      <ModalApp
+        modalIsOpen={modalIsOpen}
+        closeModal={() => setModalIsOpen(!modalIsOpen)}
+      />
+      <header>
+        <h1>Central de Controle</h1>
+
+        <section>
+          <Button
+            title="Adicionar dispositivo"
+            icon={<FiPlus color="#fff" size={20} />}
+            onClick={() => handleModal()}
+          />
+          <Button
+            title="Download logs"
+            icon={<FiDownload color="#fff" size={20} />}
+          />
+        </section>
+      </header>
     </div>
   );
 }
